9ff936aa3d551d3ad43ffe7208ea1fa14d6c06e4
Stacey Oue <stacey.oue@autodesk.com>
2020-08-13 12:00:21 -0400

    Initialize the Qt dark theme style for the running QApplication. (#16)

--- engine.py
+++ engine.py
@@ -63,6 +63,10 @@ class SketchBookEngine(Engine):
         QtCore.QTextCodec.setCodecForCStrings(utf8)
         self.logger.debug("set utf-8 codec for widget text")
 
+        # Make the QApplication use the dark theme. Must be called after the QApplication is instantiated, which
+        # is done from Sketchbook
+        self._initialize_dark_look_and_feel()
+
     def post_app_init(self):
         self.logger.debug("%s: Post app init...", self)
 
